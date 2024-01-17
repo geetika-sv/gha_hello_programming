a6be6054a96ec96013c1a7ca68dc700bc5e56377
Geetika-Jain1 <drgeetika332@gmail.com>
2024-01-18 00:01:56 +0100

    Update Action

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello welcome to the programming kurs in Jensen!");
+        System.out.println("Hello my name is Geetika!");
+        System.out.println("I am doing programming kurs in Jensen");
+        System.out.println("My teacher is helping a lot");
+        System.out.println("Today we are studing online");
     }
 }
